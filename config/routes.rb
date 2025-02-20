5b748c5cf10803a7648780c0ecdcfe85dda647fe
masato <m99234527@gmail.com>
2025-02-20 21:23:39 +0900

    コードスタイル修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   root "top#index"
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    registrations: "users/registrations",
+    sessions: "users/sessions"
   }
 end
