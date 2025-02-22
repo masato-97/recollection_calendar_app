ce994f41c9033fffdb690e02214ea4e296666a05
masato <m99234527@gmail.com>
2025-02-22 14:29:45 +0900

    コードスタイル修正

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -8,8 +8,8 @@ class MemoriesController < ApplicationController
 
   def move_to_signed_in
     unless user_signed_in?
-      #サインインしていないユーザーはログインページが表示される
-      redirect_to  "/users/sign_in"
+      # サインインしていないユーザーはログインページが表示される
+      redirect_to "/users/sign_in"
     end
   end
 end
