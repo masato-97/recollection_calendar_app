aff8f36f062c05a634079a329fe6763630d8abb6
masato <m99234527@gmail.com>
2025-03-06 20:54:03 +0900

    登録した思い出の数を表示

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module CalendarMyapp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "Tokyo"
+
+    config.beginning_of_week = :sunday
   end
 end
