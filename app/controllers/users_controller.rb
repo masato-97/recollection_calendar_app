2c6a229f2541f6e11e97fc7ae63f137f14cc4ba2
masato <m99234527@gmail.com>
2025-03-25 19:40:59 +0900

    UsersController修正、UsersControllerのテストコード修正３

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+  before_action :move_to_signed_in
+  
   def show
     @user = current_user
+  end
+
+  private
+
+  def move_to_signed_in
+    unless user_signed_in?
+      # サインインしていないユーザーはログインページが表示される
+      redirect_to "/users/sign_in"
+    end
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,13 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get show" do
-    user = User.create(name: "テストユーザー", email: "test@example.com", password: "password")
-    log_in(user)
-    get user_url(user)
+    user = User.create!(name: "テストユーザー", email: "test@example.com", password: "password")
+    sign_in user # current_user を設定
+
+    get user_url(user) # 適切なルートに変更
     assert_response :success
   end
 end
