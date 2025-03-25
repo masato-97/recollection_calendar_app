5b8dbe0854346c559ea6583c9ee150e43f93ba74
masato <m99234527@gmail.com>
2025-03-25 19:29:54 +0900

    UsersControllerのテストコード修正２

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    user = User.create(name: "テストユーザー", email: "test@example.com")
-    Memory.create(title: "テストのタイトル", body: "テストのボディ", user_id: user.id)
+    user = User.create(name: "テストユーザー", email: "test@example.com", password: "password")
+    log_in(user)
     get user_url(user)
     assert_response :success
   end
