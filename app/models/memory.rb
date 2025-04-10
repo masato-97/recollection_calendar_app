ab565b65028f62a1ef321641c022ec3abad1bc9d
masato <m99234527@gmail.com>
2025-04-10 13:36:52 +0900

    コードスタイル修正

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -9,39 +9,39 @@ class Memory < ApplicationRecord
   has_one_attached :main_image
   has_one_attached :sub_image
 
-  scope :memories_this_month, -> (user_id, start_date) {
+  scope :memories_this_month, ->(user_id, start_date) {
     where(user_id: user_id, day: start_date.beginning_of_month..start_date.end_of_month)
   }
-  scope :memories_last_month, -> (user_id, start_date) {
+  scope :memories_last_month, ->(user_id, start_date) {
     last_month_start = start_date.last_month.beginning_of_month
     last_month_end = start_date.last_month.end_of_month
     where(user_id: user_id, day: last_month_start..last_month_end)
   }
-  scope :memories_2month_ago, -> (user_id, start_date) {
+  scope :memories_2month_ago, ->(user_id, start_date) {
     two_month_ago = start_date.last_month.last_month
     last_month_start = two_month_ago.beginning_of_month
     last_month_end = two_month_ago.end_of_month
     where(user_id: user_id, day: last_month_start..last_month_end)
   }
-  scope :memories_3month_ago, -> (user_id, start_date) {
+  scope :memories_3month_ago, ->(user_id, start_date) {
     three_month_ago = start_date.last_month.last_month.last_month
     last_month_start = three_month_ago.beginning_of_month
     last_month_end = three_month_ago.end_of_month
     where(user_id: user_id, day: last_month_start..last_month_end)
   }
-  scope :memories_4month_ago, -> (user_id, start_date) {
+  scope :memories_4month_ago, ->(user_id, start_date) {
     four_month_ago = start_date.last_month.last_month.last_month.last_month
     last_month_start = four_month_ago.beginning_of_month
     last_month_end = four_month_ago.end_of_month
     where(user_id: user_id, day: last_month_start..last_month_end)
   }
-  scope :memories_5month_ago, -> (user_id, start_date) {
+  scope :memories_5month_ago, ->(user_id, start_date) {
     five_month_ago = start_date.last_month.last_month.last_month.last_month.last_month
     last_month_start = five_month_ago.beginning_of_month
     last_month_end = five_month_ago.end_of_month
     where(user_id: user_id, day: last_month_start..last_month_end)
   }
-  scope :memories_6month_ago, -> (user_id, start_date) {
+  scope :memories_6month_ago, ->(user_id, start_date) {
     six_month_ago = start_date.last_month.last_month.last_month.last_month.last_month.last_month
     last_month_start = six_month_ago.beginning_of_month
     last_month_end = six_month_ago.end_of_month
