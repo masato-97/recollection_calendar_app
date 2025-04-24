f420c6fa87535f770662c634588e1fe66136b55c
masato <m99234527@gmail.com>
2025-04-24 15:52:16 +0900

    holiday_mailer_preview.rb のコードスタイル修正

--- test/mailers/previews/holiday_mailer_preview.rb
+++ test/mailers/previews/holiday_mailer_preview.rb
@@ -2,6 +2,6 @@
 class HolidayMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/holiday_mailer/holiday_notification
   # def holiday_notification
-    # HolidayMailer.holiday_notification
+  #   HolidayMailer.holiday_notification
   # end
 end
