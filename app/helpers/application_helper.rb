107a4c81f795e64345686eeaa78808d358c230ad
masato <m99234527@gmail.com>
2025-05-23 20:19:24 +0900

    GA4の設定、タブにページタイトルを表示、OGP修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
+  def page_title(title = "")
+    base_title = "Recordi"
+    title.present? ? "#{title} - #{base_title}" : base_title
+  end
+
   def default_meta_tags
     {
-      site: "Recordi（レコルディ）",
-      title: "思い出の記録・評価・共有ができるサービス",
-      reverse: true,
-      charset: "utf-8",
-      description: "Recordi（レコルディ）とは、思い出の記録、1〜5個の星で思い出を評価、思い出を他のユーザーに共有することができるサービスです。思い出の記録だけでなく、場所、作品、料理などを記録しレビューアプリとして使用することもできます。",
+      description: "思い出の記録・評価・共有ができるWebアプリです。Recordi（レコルディ）で、思い出を記録し、1〜5個の星で思い出を評価し、他の人と共有しましょう。思い出の記録だけでなく、場所、作品、料理などを記録しレビューアプリとして使用することもできます。",
       keywords: "思い出,旅行,料理,グルメ,映画,本,漫画,レビュー,共有",
       canonical: "https://recordi.jp",
-      separator: "|",
       og: {
-        site_name: :site,
+        site_name: "Recordi",
         title: :title,
         description: :description,
         type: "website",
