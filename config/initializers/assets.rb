936fe94be62dbd6f5aa6651093376f409f61523a
masato <m99234527@gmail.com>
2025-08-05 14:32:01 +0900

    コードスタイル修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,5 +7,5 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 
-Rails.application.config.assets.precompile += %w( application.js )
+Rails.application.config.assets.precompile += %w[application.js]
 Rails.application.config.assets.precompile += %w[rating.js]
